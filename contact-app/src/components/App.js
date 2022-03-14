32411a36f57b95c4e27fc307cde08a053093ff26
NotRayhaan <rayhaanmohammad@gmail.com>
2022-03-14 15:27:52 +1300

    1.1
    ability to add contacts, and save to local storage

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
+import { useState } from 'react';
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+  
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  };
 
-  const contacts = [
-    {
-      id: "1",
-      name: "Rayhaan",
-      email: "rayhaanmohammad@gmail.com"
-    },
-    {
-      id: "2",
-      name: "john",
-      email: "junfan@gmail.com"
-    }
-  ];
+  useEffect(() => {
+    const retrieveContacts =  JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(retrieveContacts) setContacts(retrieveContacts);
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  },[contacts])
+
   return (
     <div className="ui container">
 
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={contacts}/>
     </div>
   );
